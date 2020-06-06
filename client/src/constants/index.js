598e7b4945e2a667a0cc0ae5b001201a4b2dbf7b
Glen Lindquist <glenlindquist@gmail.com>
2020-06-06 15:25:41 -0500

    wss vs ws

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://bbq-bud.herokuapp.com';
-export const API_WS_ROOT = 'ws://bbq-bud.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://bbq-bud.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
